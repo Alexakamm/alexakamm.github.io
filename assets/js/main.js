d23fc820bc9d7e93e3a5f96d34b678cfe9d87460
Alexakamm <alexakamm19@gmail.com>
2026-04-09 20:27:37 -0400

    Cleanup sections, add resume, styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -224,168 +224,118 @@
 
 				});
 
-		// Details buttons. *** FIX ADDED HERE ***
-			$('.gallery').on('click', '.button', function(event) {
-
-				event.stopPropagation();
-				event.preventDefault();
-
-				var $btn     = $(this);
-				var $caption = $btn.closest('.caption');
-				var title    = $caption.find('h3').text();
-				var body     = $caption.find('p').text();
-
-				// Build modal once if it doesn't exist yet
-				if ($('#exp-modal').length === 0) {
-					$('body').append(
-						'<div id="exp-modal" style="' +
-							'display:none; position:fixed; top:0; left:0;' +
-							'width:100%; height:100%; z-index:999999;' +
-							'background:rgba(0,0,0,0.75);' +
-							'align-items:center; justify-content:center;">' +
-							'<div style="' +
-								'background:#fff; color:#111; padding:2.5em;' +
-								'max-width:480px; width:90%; border-radius:8px;' +
-								'position:relative; box-shadow:0 10px 40px rgba(0,0,0,0.4);">' +
-								'<h3 id="exp-title" style="margin-bottom:0.5em; font-size:1.2em;"></h3>' +
-								'<p id="exp-body" style="line-height:1.6;"></p>' +
-								'<button id="exp-close" style="' +
-									'margin-top:1.5em; padding:0.5em 1.5em;' +
-									'background:#000; color:#fff; border:none;' +
-									'border-radius:4px; cursor:pointer; font-size:0.9em;">' +
-									'Close' +
-								'</button>' +
-							'</div>' +
-						'</div>'
-					);
-
-					$('#exp-close').on('click', function() {
-						$('#exp-modal').css('display', 'none');
-					});
-
-					$('#exp-modal').on('click', function(e) {
-						if (e.target === this) $(this).css('display', 'none');
-					});
-				}
-
-				$('#exp-title').text(title);
-				$('#exp-body').text(body);
-				$('#exp-modal').css('display', 'flex');
-
-			});
-
 		// Lightbox.
 			$('.gallery.lightbox')
-				.on('click', 'a', function(event) {
+				// .on('click', 'a', function(event) {
 
-					var $a = $(this),
-						$gallery = $a.parents('.gallery'),
-						$modal = $gallery.children('.modal'),
-						$modalImg = $modal.find('img'),
-						href = $a.attr('href');
+				// 	var $a = $(this),
+				// 		$gallery = $a.parents('.gallery'),
+				// 		$modal = $gallery.children('.modal'),
+				// 		$modalImg = $modal.find('img'),
+				// 		href = $a.attr('href');
 
-					// Not an image? Bail.
-						if (!href.match(/\.(jpg|gif|png|mp4)$/))
-							return;
+				// 	// Not an image? Bail.
+				// 		if (!href.match(/\.(jpg|gif|png|mp4)$/))
+				// 			return;
 
-					// Prevent default.
-						event.preventDefault();
-						event.stopPropagation();
+				// 	// Prevent default.
+				// 		event.preventDefault();
+				// 		event.stopPropagation();
 
-					// Locked? Bail.
-						if ($modal[0]._locked)
-							return;
+				// 	// Locked? Bail.
+				// 		if ($modal[0]._locked)
+				// 			return;
 
-					// Lock.
-						$modal[0]._locked = true;
+				// 	// Lock.
+				// 		$modal[0]._locked = true;
 
-					// Set src.
-						$modalImg.attr('src', href);
+				// 	// Set src.
+				// 		$modalImg.attr('src', href);
 
-					// Set visible.
-						$modal.addClass('visible');
+				// 	// Set visible.
+				// 		$modal.addClass('visible');
 
-					// Focus.
-						$modal.focus();
+				// 	// Focus.
+				// 		$modal.focus();
 
-					// Delay.
-						setTimeout(function() {
+				// 	// Delay.
+				// 		setTimeout(function() {
 
-							// Unlock.
-								$modal[0]._locked = false;
+				// 			// Unlock.
+				// 				$modal[0]._locked = false;
 
-						}, 600);
+				// 		}, 600);
 
-				})
-				.on('click', '.modal', function(event) {
+				// })
+				// .on('click', '.modal', function(event) {
 
-					var $modal = $(this),
-						$modalImg = $modal.find('img');
+				// 	var $modal = $(this),
+				// 		$modalImg = $modal.find('img');
 
-					// Locked? Bail.
-						if ($modal[0]._locked)
-							return;
+				// 	// Locked? Bail.
+				// 		if ($modal[0]._locked)
+				// 			return;
 
-					// Already hidden? Bail.
-						if (!$modal.hasClass('visible'))
-							return;
+				// 	// Already hidden? Bail.
+				// 		if (!$modal.hasClass('visible'))
+				// 			return;
 
-					// Lock.
-						$modal[0]._locked = true;
+				// 	// Lock.
+				// 		$modal[0]._locked = true;
 
-					// Clear visible, loaded.
-						$modal
-							.removeClass('loaded')
+				// 	// Clear visible, loaded.
+				// 		$modal
+				// 			.removeClass('loaded')
 
-					// Delay.
-						setTimeout(function() {
+				// 	// Delay.
+				// 		setTimeout(function() {
 
-							$modal
-								.removeClass('visible')
+				// 			$modal
+				// 				.removeClass('visible')
 
-							setTimeout(function() {
+				// 			setTimeout(function() {
 
-								// Clear src.
-									$modalImg.attr('src', '');
+				// 				// Clear src.
+				// 					$modalImg.attr('src', '');
 
-								// Unlock.
-									$modal[0]._locked = false;
+				// 				// Unlock.
+				// 					$modal[0]._locked = false;
 
-								// Focus.
-									$body.focus();
+				// 				// Focus.
+				// 					$body.focus();
 
-							}, 475);
+				// 			}, 475);
 
-						}, 125);
+				// 		}, 125);
 
-				})
-				.on('keypress', '.modal', function(event) {
+				// })
+				// .on('keypress', '.modal', function(event) {
 
-					var $modal = $(this);
+				// 	var $modal = $(this);
 
-					// Escape? Hide modal.
-						if (event.keyCode == 27)
-							$modal.trigger('click');
+				// 	// Escape? Hide modal.
+				// 		if (event.keyCode == 27)
+				// 			$modal.trigger('click');
 
-				})
-				.prepend('<div class="modal" tabIndex="-1"><div class="inner"><img src="" /></div></div>')
-					.find('img')
-						.on('load', function(event) {
+				// })
+				// .prepend('<div class="modal" tabIndex="-1"><div class="inner"><img src="" /></div></div>')
+				// 	.find('img')
+				// 		.on('load', function(event) {
 
-							var $modalImg = $(this),
-								$modal = $modalImg.parents('.modal');
+				// 			var $modalImg = $(this),
+				// 				$modal = $modalImg.parents('.modal');
 
-							setTimeout(function() {
+				// 			setTimeout(function() {
 
-								// No longer visible? Bail.
-									if (!$modal.hasClass('visible'))
-										return;
+				// 				// No longer visible? Bail.
+				// 					if (!$modal.hasClass('visible'))
+				// 						return;
 
-								// Set loaded.
-									$modal.addClass('loaded');
+				// 				// Set loaded.
+				// 					$modal.addClass('loaded');
 
-							}, 275);
+				// 			}, 275);
 
-						});
+				// 		});
 
 })(jQuery);
